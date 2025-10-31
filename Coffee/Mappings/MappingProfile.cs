d3f76e7692fdad6f87bb569fcbc4c5012a1f8951
NTC <cuongnt2996@gmail.com>
2025-10-31 15:53:39 +0700

    implement auto mapper

--- Coffee/Mappings/MappingProfile.cs
+++ Coffee/Mappings/MappingProfile.cs
@@ -2,10 +2,38 @@ using AutoMapper;
 using Coffee.Models;
 using Coffee.Models.DTOs;
 namespace Coffee.Mappings;
+
 public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        CreateMap<Employee, EmployeeDto>().ReverseMap();
+        // CreateMap<Employee, EmployeeDto>()
+        // .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+        // .ReverseMap()
+        // .ForMember(dest => dest.FirstName,
+        //         opt => opt.MapFrom(src => src.FullName.Split(' ')[0]))
+        //         .ForMember(dest => dest.LastName,
+        //                opt => opt.MapFrom(src => src.FullName.Split(' ').Length > 1
+        //                                          ? src.FullName.Split(' ')[1]
+        //                                          : string.Empty));
+
+    CreateMap<Employee, EmployeeDto>()
+    .ForMember(dest => dest.FullName,
+               opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+    .ReverseMap()
+    .ForMember(dest => dest.FirstName,
+               opt => opt.MapFrom(src =>
+                   string.IsNullOrWhiteSpace(src.FullName)
+                       ? string.Empty
+                       : src.FullName.Split(' ', StringSplitOptions.RemoveEmptyEntries).First()))
+    .ForMember(dest => dest.LastName,
+               opt => opt.MapFrom(src =>
+                   string.IsNullOrWhiteSpace(src.FullName)
+                       ? string.Empty
+                       : string.Join(" ",
+                           src.FullName.Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1))));
+
+
+
     }
 }
